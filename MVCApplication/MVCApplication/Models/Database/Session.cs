d5c9180ac329876bd7e213ac38adaf67d13d9517
MMichael <mmichael@advancedsolutions.com>
2015-01-29 23:09:33 -0500

    Started session auth login with postgres

---
+++ MVCApplication/MVCApplication/Models/Database/Session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCApplication.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+
+        //Indexing on userId
+        //If user has session id cached(cookies) then try to auth with that else request login.
+        public int UserId { get; set; }
+
+        public DateTime LastRequest { get; set; }
+    }
+}
